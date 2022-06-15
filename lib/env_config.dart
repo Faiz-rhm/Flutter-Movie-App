d3d7d3c534017f41b9292c8392af727da95d6d68
Rahman <faiz199011@gmail.com>
2022-06-15 16:33:33 +0430

    Update URL

--- lib/env_config.dart
+++ lib/env_config.dart
@@ -4,9 +4,12 @@ class EnvironmentConfig {
       defaultValue: "https://api.themoviedb.org/3/");
   static
   const IMAGE_BASE_URL = String.fromEnvironment('IMAGE_BASE_URL',
-      defaultValue: "https://image.tmdb.org/t/p/w185/");
+      defaultValue: "https://image.tmdb.org/t/p/w300_and_h450_bestv2/");
+  static
+  const IMAGE_BASE_URL_COVER = String.fromEnvironment('IMAGE_BASE_URL',
+      defaultValue: "https://image.tmdb.org/t/p/w355_and_h200_multi_faces/");
   static
   const API_KEY = String.fromEnvironment('API_KEY',
       //Todo put your api key here
-      defaultValue: "YOUR_API_KEY");
+      defaultValue: "");
 }
