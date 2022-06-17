a6e2a38d3ad4ff4705eb209f5fa67c754e9f3d91
Rahman <faiz199011@gmail.com>
2022-06-17 20:39:00 +0430

    Add movie detail  provider

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/extension.dart';
+import 'package:movie_app/model/MovieDetails/MovieDetails.dart';
 import 'package:movie_app/model/MovieResponse/MovieResponse.dart';
 
 import 'env_config.dart';
@@ -17,12 +18,13 @@ final movieTypeProvider = StateProvider((ref)=>MoviesType.popular);
 final moviesProvider = FutureProvider < List < Movie >> ((ref) async {
   final movieType = ref.watch(movieTypeProvider);
   final dio = ref.watch(dioProvider);
-  print('dio $dio');
+  print("movieType: $movieType");
   final response = await dio.get('movie/'+movieType.value,
     queryParameters: {
       'api_key': EnvironmentConfig.API_KEY
     }
   );
+  print('response: ${response.data}');
   return MovieResponse.fromJson(response.data).results!;
 });
 
@@ -36,14 +38,19 @@ final upcomingProvider = FutureProvider < List < Movie >> ((ref) async {
   return MovieResponse.fromJson(response.data).results!;
 });
 
-// final movieDetailsProvider = FutureProvider < List < Movie >> ((ref) async {
-//   final dio = ref.watch(dioProvider);
-//   final response = await dio.get('movie/'+movieID,
-//     queryParameters: {
-//       'api_key': EnvironmentConfig.API_KEY
-//     }
-//   );
-//   return MovieResponse.fromJson(response.data).results!;
-// });
+final movieIDProvider = StateProvider<int>((ref)=> 0);
+
+final movieDetailsProvider = FutureProvider <MovieDetails>((ref) async {
+  final movieID = ref.watch(movieIDProvider);
+  print('movieID $movieID');
+  final dio = ref.watch(dioProvider);
+  final response = await dio.get('movie/'+movieID.toString(),
+    queryParameters: {
+      'api_key': EnvironmentConfig.API_KEY
+    }
+  );
+  print('response ${response.data}');
+  return MovieDetails.fromJson(response.data);
+});
 
 final movieProvider = Provider((ref) => throw UnimplementedError());
