1fb19dcd60b71b2c4a4d0d1d53c121fdd30cd9cd
Rahman <faiz199011@gmail.com>
2022-06-19 22:36:16 +0430

    Add recommended

---
+++ lib/view/widgets/recommended.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_app/env_config.dart';
+import 'package:movie_app/model/Movie/Movie.dart';
+import 'package:movie_app/provider.dart';
+
+class RecommendedWidget extends ConsumerWidget {
+  const RecommendedWidget({Key? key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final casts = ref.watch(upcomingProvider);
+    print('casts: $casts');
+    return casts.maybeWhen(
+      orElse: () => const Center(child: Text('Or Lese')),
+      loading: () => const Center(child: CircularProgressIndicator()),
+      data: (data) => _buildCastList(context, data),
+    );
+  }
+
+  _buildCastList(BuildContext context, List<Movie> data) {
+    final theme = Theme.of(context);
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          final movie = data.elementAt(index);
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 100,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                         '${EnvironmentConfig.IMAGE_BASE_URL}${movie.poster_path}',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                SizedBox(
+                  width: 100,
+                  child: Text(
+                    movie.title,
+                    style: theme.textTheme.subtitle2,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
