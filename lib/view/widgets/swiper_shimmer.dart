2a6fc15a00f8e767143d2a45b393ecab4f13698f
Rahman <faiz199011@gmail.com>
2022-06-20 22:46:52 +0430

    Add  Shimmer

---
+++ lib/view/widgets/swiper_shimmer.dart
@@ -0,0 +1,85 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/material.dart';
+import 'package:skeletons/skeletons.dart';
+
+class SwiperShimmer extends StatelessWidget {
+  const SwiperShimmer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 350,
+      child: Swiper(
+        itemCount: 3,
+        viewportFraction: 0.42,
+        scale: 0.5,
+        itemBuilder: (BuildContext context, int index) {
+          final movie = index;
+          return Container(
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.1),
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                  ),
+                  height: 260,
+                  child: SkeletonAvatar(
+                    style: SkeletonAvatarStyle(
+                      width: double.infinity,
+                      minHeight: MediaQuery.of(context).size.height / 8,
+                      maxHeight: MediaQuery.of(context).size.height / 3,
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 5,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SkeletonAvatar(
+                      style: SkeletonAvatarStyle(
+                        height: 30,
+                        width: 40,
+                        borderRadius: BorderRadius.circular(10),
+                        padding: const EdgeInsetsDirectional.only(start: 5)
+                      ),
+                    ),
+                    SkeletonAvatar(
+                      style: SkeletonAvatarStyle(
+                        height: 30,
+                        width: 50,
+                        borderRadius: BorderRadius.circular(10),
+                        padding: const EdgeInsetsDirectional.only(start: 5)
+                      ),
+                    ),
+                    SkeletonAvatar(
+                      style: SkeletonAvatarStyle(
+                        height: 30,
+                        width: 45,
+                        borderRadius: BorderRadius.circular(10),
+                        padding: const EdgeInsetsDirectional.only(start: 5)
+                      ),
+                    ),
+                  ],
+                ),
+                SkeletonLine(
+                  style: SkeletonLineStyle(
+                    padding: const EdgeInsets.all(10.0),
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/view/widgets/upcomming_shimmere.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:skeletons/skeletons.dart';
+
+class UpcomingShimmer extends StatelessWidget {
+  const UpcomingShimmer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 16, left: 16, bottom: 16),
+      height: 250,
+      width: double.infinity,
+      child: Container(
+        width: MediaQuery.of(context).size.height,
+        margin: const EdgeInsets.only(right: 20),
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.1),
+          borderRadius: const BorderRadius.all(Radius.circular(30)),
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(15),
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(5))
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 5,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SkeletonParagraph(
+                      style: SkeletonParagraphStyle(
+                        lines: 2,
+                        spacing: 9,
+                        lineStyle: SkeletonLineStyle(
+                          randomLength: true,
+                          height: 15,
+                          borderRadius: BorderRadius.circular(8),
+                          minLength: MediaQuery.of(context).size.width / 6,
+                          maxLength: MediaQuery.of(context).size.width / 3,
+                        )
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SkeletonAvatar(
+                style: SkeletonAvatarStyle(shape: BoxShape.circle, width: 50, height: 50),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
