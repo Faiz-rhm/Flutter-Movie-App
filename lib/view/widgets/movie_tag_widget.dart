7e2f14131dd969cae60055b38e9c986523aa3a17
Rahman <faiz199011@gmail.com>
2022-06-20 22:47:24 +0430

    Change color

--- lib/view/widgets/movie_tag_widget.dart
+++ lib/view/widgets/movie_tag_widget.dart
@@ -23,7 +23,7 @@ class MovieTags extends ConsumerWidget {
                 label: Text(
                   type.name,
                 ),
-                backgroundColor: type == movieType ? Colors.red.shade900 : Colors.black.withOpacity(0.6),
+                backgroundColor: type == movieType ? Colors.red.shade900 : Colors.grey.withOpacity(0.1),
               ),
             ),
           ))
