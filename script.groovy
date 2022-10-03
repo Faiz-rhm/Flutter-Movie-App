8d68e36b12487a95fd4cfa9b8ee61c8337c18469
Rahman <faiz199011@gmail.com>
2022-10-03 13:45:37 +0430

    Update jenkins

--- script.groovy
+++ script.groovy
@@ -1,15 +1,15 @@
 def buildApp() {
-    echo "Building app...";
+    echo 'Building app...'
 }
 
 def testingApp() {
-    echo "Testing app...";
-    sh 'flutter test';
+    echo 'Testing app...'
+    sh 'flutter test'
 }
 
 def deployingApp() {
-    echo "Deploying app...";
-    sh 'flutter build apk';
+    echo 'Deploying app...'
+    sh 'flutter build apk'
 }
 
 return this
